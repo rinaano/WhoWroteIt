d5fb4c4778f1b79f5875db1416fe90f286f9ecef
rinaano <frina48@gmail.com>
2018-10-24 13:37:48 +0700

    WhoWroteIt

---
+++ app/src/main/java/com/example/farahnisrinahs/whowroteit/BookLoader.java
@@ -0,0 +1,31 @@
+package com.example.farahnisrinahs.whowroteit;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+    @Nullable
+    @Override
+    public String loadInBackground(){
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+    public BookLoader(@NonNull Context context){
+        super (context);
+    }
+
+    @Override
+    protected void onStartLoading(){
+        super.onStartLoading();
+    }
+
+    private String mQueryString;
+
+    BookLoader(Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+}
